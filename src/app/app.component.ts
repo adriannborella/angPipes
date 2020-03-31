cea3514e503a124cd321d8eec9a38ab417b64122
Adrian Casa <aborella@anacsoft.com>
2020-03-31 16:04:04 -0300

    Pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipes';
+  nombre = 'Adrián Borella';
+
+  arreglo = [1, 2, 3, 4, 5, 6];
+
+  PI = Math.PI;
+
+  porcentaje: number = 0.76;
+
+  total:number = 1389.78;
+
+  data = {
+    'nombre': 'Adrian',
+    'apellido': 'Borella'
+  };
+
+  valorPromesa = new Promise<string>( 
+    resolve => {
+      setTimeout(
+        () => {
+          resolve('llego la data');
+        }
+      , 4500);
+    }
+  );
+
+  fecha = new Date();
+
+  idioma: string = 'es';
+
+  changeLanguague(lan) {
+    this.idioma = lan;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 
 import { AppComponent } from './app.component';
+
+import { registerLocaleData } from '@angular/common';
+import localEs from "@angular/common/locales/es";
+import localEn from "@angular/common/locales/en";
+
+registerLocaleData(localEs, 'es');
+registerLocaleData(localEn, 'en');
 
 @NgModule({
   declarations: [
@@ -10,7 +17,12 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: LOCALE_ID,
+      useValue: 'es'
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
